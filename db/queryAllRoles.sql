073530d5791627f9751899c9c576e206786e2241
Code Queen <channguyen97@gmail.com>
2023-10-04 19:55:15 -0700

    removed files for draft code

--- db/queryAllRoles.sql
+++
@@ -1,7 +0,0 @@
--- View all Roles should show: job title, role id, the department that role belongs to, and the salary
-
-SELECT title, salary, employee_role.id, department_name FROM department JOIN employee_role ON department.id = employee_role.department_id;
-
-
-
-
--- db/queryUpdateEmployees.sql
+++
@@ -1,8 +0,0 @@
-
--- UPDATE employee SET (employee_role_id) VALUE(?), WHERE employee.employee_role_id = employee_role.id
-UPDATE employee SET (emoployee_role_id) VALUES(?, ?, ?)
-
-
-UPDATE Station AS st1, StationOld AS st2
-   SET st1.already_used = 1
- WHERE st1.code = st2.code
